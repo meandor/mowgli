92cc075b0642f3bd5848dc44161807d5f5116b32
Daniel Schruhl <danielschruhl@gmail.com>
2020-03-13 16:15:26 +0100

    Add classification model

--- mowgli/model.py
+++ mowgli/model.py
@@ -1,11 +1,32 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from tensorflow import keras
 
 
 def _extract_features(_labels, features):
     return features
 
 
-def train_vectorizer(dataset):
-    vectorizer = CountVectorizer(preprocessor=lambda x: str(x.numpy()))
+def train_vectorizer(dataset, vocabulary_size):
+    vectorizer = CountVectorizer(max_features=vocabulary_size, preprocessor=lambda x: str(x.numpy()))
     vectorizer.fit(dataset.map(_extract_features))
     return vectorizer
+
+
+def classification_model(vocabulary_size, embedding_dimension, labels_count):
+    input_layer = keras.Input(shape=(vocabulary_size,), dtype='int32', name='word_vector_input')
+    embedding_layer = keras.layers.Embedding(
+        input_dim=vocabulary_size,
+        output_dim=embedding_dimension,
+        name='embedding_layer'
+    )(input_layer)
+
+    layer1 = keras.layers.GlobalAveragePooling1D()(embedding_layer)
+    layer2 = keras.layers.Dense(100, activation='relu')(layer1)
+    layer3 = keras.layers.Dense(25, activation='relu')(layer2)
+
+    output_layer = keras.layers.Dense(
+        labels_count,
+        activation='softmax',
+        name='classification_output'
+    )(layer3)
+    return keras.Model(inputs=input_layer, outputs=output_layer)
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from sklearn.feature_extraction.text import CountVectorizer
+from tensorflow.keras import Model
 
 from mowgli import model, datasets
 
@@ -7,7 +8,7 @@ from mowgli import model, datasets
 def test_should_return_vectorizer():
     given_dataset = datasets.load_dataset('tests/resources/dataset.csv')
 
-    actual = model.train_vectorizer(given_dataset)
+    actual = model.train_vectorizer(given_dataset, 1337)
     expected_type = CountVectorizer
     expected_features = ['bar', 'foo', 'foobar', 'spaghetti']
 
@@ -18,8 +19,15 @@ def test_should_return_vectorizer():
 def test_should_train_vectorizer():
     given_dataset = datasets.load_dataset('tests/resources/dataset.csv')
 
-    vectorizer = model.train_vectorizer(given_dataset)
+    vectorizer = model.train_vectorizer(given_dataset, 4)
     actual = vectorizer.transform(tf.constant(['foo'])).toarray()
     expected = [0, 1, 0, 0]
 
     assert (expected == actual).all
+
+
+def test_should_return_model():
+    actual = type(model.classification_model(1000, 16, 10))
+    expected = Model
+
+    assert expected == actual
