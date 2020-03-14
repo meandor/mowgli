3635834dabcd17634c8fda39b0763d75e838b061
Daniel Schruhl <danielschruhl@gmail.com>
2020-03-14 10:15:20 +0100

    Fix linter

--- mowgli/__init__.py
+++ mowgli/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 logging.basicConfig(
-    format="%(asctime)s %(levelname)s	%(message)s [%(process)d] %(module)s %(filename)s %(funcName)s",
+    format="%(asctime)s %(levelname)s	%(message)s "
+           "[%(process)d] %(module)s %(filename)s %(funcName)s",
     level=os.environ.get("LOGLEVEL", "DEBUG")
 )
